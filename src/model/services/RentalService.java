e3a047945d148e8611d1c5b83f4a68c3a897ff5b
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2020-11-30 12:36:27 -0300

    Problem solving - part 1

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,22 @@
+package model.services;
+
+import model.entities.CarRental;
+
+public class RentalService {
+	
+	public Double pricePerHour;
+	public Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		super();
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+	}
+}
